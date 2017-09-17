b6f003aba74de36c7d4f1e61fbe1d2d3d42113f4
agus24 <agusx244@gmail.com>
2017-09-17 21:54:19 +0700

    fix next music bug

--- index.view.php
+++ index.view.php
@@ -46,56 +46,7 @@
     </div>
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/bootstrap.js"></script>
-<script type="text/javascript">
-    var music = <?=json_encode($files)?>;
-    var np = new Audio;
-    function stop() {
-        np.pause();
-        np.currentTime = 0;
-    }
-    function resume() {
-        np.play();
-    }
-
-    function pause() {
-        np.pause();
-    }
-
-    function play(index) {
-        np.pause();
-        np.src = music[index];
-        np.play();
-        setInterval(function() {
-            var duration = parseTime(np.duration);
-            var currentTime = parseTime(np.currentTime);
-            $('#time').text(currentTime+"/"+duration);
-            if(np.currentTime == np.duration) {
-                if(music.length == index+1) {
-                    play(0);
-                } else {
-                    play(index+1);
-                }
-            }
-        }, 1000)
-        $('#nowPlaying').empty();
-        var text = music[index].split('music/')[1];
-        $('#nowPlaying').text(text);
-        $('li').each(function() {
-            $(this).removeClass('play');
-            if($(this).data('index') == index) {
-                $(this).addClass('play');
-            }
-        });
-    }
-
-    function parseTime(audio) {
-        var mins = Math.floor(audio / 60);
-        var secs = Math.floor(audio % 60);
-        if (secs < 10) {
-        secs = '0' + String(secs);
-        }
-        return (mins + ':' + secs);
-    }
-</script>
+<script>var music = <?=json_encode($files)?>;</script>
+<script type="text/javascript" src="js/player.js"></script>
 </body>
 </html>
